efd73e1151aa65b9be75e38c8458bc99b771e2b0
Djamel Nait Sidenas <xamel.ns@gmail.com>
2025-02-15 01:05:15 -0500

    refactor : move menu to controller

--- src/Main.java
+++ src/Main.java
@@ -5,76 +5,14 @@ import view.LivreView;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try {
             LivreView view = new LivreView();
             LivreController controller = new LivreController("src/data/livres.csv", view);
-            Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            controller.start();
 
-            while (true) {
-                view.afficherMenu();
-
-                int choix = ESUtilisateur.getInt(1,11,"Choisissez une option : ");
-
-                switch (choix) {
-                    case 1:
-                        String categorie = ESUtilisateur.getText("Quelle catégorie ? : ");
-                        controller.afficherParCategorie(categorie);
-                        break;
-                    case 2:
-
-                        String auteur = ESUtilisateur.getText("Quel est le nom de l'auteur ? : ");
-                        controller.afficherParAuteur(auteur);
-                        break;
-                    case 3:
-                        String titre = ESUtilisateur.getText("Quel est le titre du livre ? : ");
-                        controller.afficherParTitre(titre);
-                        break;
-                    case 4:
-                        LocalDate datePublication = ESUtilisateur.getDate("Entrez la date de publication (yyyy-MM-dd): ");
-                        controller.afficherParDatePublication(datePublication);
-                        break;
-                    case 5:
-                        double prix = ESUtilisateur.getDouble(0,Double.MAX_VALUE,"Quel est le prix ? : ");
-                        controller.afficherParPrixExact(prix);
-                        break;
-                    case 6:
-                        controller.afficherLivresRuptureStock();
-                        break;
-                    case 7:
-                        String auteurSpecifique = ESUtilisateur.getText("Entrez l'auteur: ");
-                        String debutTitre = ESUtilisateur.getText("Entrez les lettres de début du titre: ");
-                        controller.afficherParAuteurEtTitre(auteurSpecifique, debutTitre);
-                        break;
-                    case 8:
-                        String auteurDate = ESUtilisateur.getText("Entrez l'auteur: ");
-                        LocalDate datePublicationAuteur = ESUtilisateur.getDate("Quelle date de publication ? :");
-                        controller.afficherParAuteurEtDate(auteurDate, datePublicationAuteur);
-                        break;
-                    case 9:
-                        String categoriePourPrix = ESUtilisateur.getText("Entrez la catégorie: ");
-                        double prixCategorieMin = ESUtilisateur.getDouble(0,Double.MAX_VALUE,"Entrez le prix minimum: ");
-                        double prixCategorieMax = ESUtilisateur.getDouble(0,Double.MAX_VALUE,"Entrez le prix minimum: ");
-                        controller.afficherParCategorieEtPrix(categoriePourPrix, prixCategorieMin, prixCategorieMax);
-                        break;
-                    case 10:
-                        controller.afficherLivresHierarchie();
-                        break;
-                    case 11:
-                        System.out.println("Au revoir!");
-                        scanner.close();
-                        System.exit(0);
-                        break;
-                    default:
-                        System.out.println("Option invalide, veuillez réessayer.");
-                }
-            }
         } catch (IOException e) {
             System.err.println("Erreur lors de la lecture du fichier : " + e.getMessage());
         }
--- src/controller/LivreController.java
+++ src/controller/LivreController.java
@@ -3,6 +3,7 @@ package controller;
 import model.ArbreAVL;
 import model.Livre;
 import util.CSVReader;
+import util.ESUtilisateur;
 import view.LivreView;
 
 import java.io.IOException;
@@ -37,32 +38,42 @@ public class LivreController {
         }
     }
 
-    public void afficherParCategorie(String categorie) {
+    public void afficherParCategorie() {
+        String categorie = ESUtilisateur.getText("Quelle catégorie ? : ");
         List<Livre> result = arbreCategorie.rechercherParCritere(Livre::getCategorie, categorie);
         view.afficherLivres(result);
     }
 
-    public void afficherParAuteur(String auteur) {
+    public void afficherParAuteur() {
+        String auteur = ESUtilisateur.getText("Quel est le nom de l'auteur ? : ");
         List<Livre> result = arbreAuteur.rechercherParCritere(Livre::getAuteur, auteur);
         view.afficherLivres(result);
     }
 
-    public void afficherParTitre(String titre) {
+    public void afficherParTitre() {
+        String titre = ESUtilisateur.getText("Quel est le titre du livre ? : ");
         List<Livre> result = arbreTitre.rechercherParCritere(Livre::getTitre, titre);
         view.afficherLivres(result);
     }
 
-    public void afficherParDatePublication(LocalDate datePublication) {
+    public void afficherParDatePublication() {
+        LocalDate datePublication = ESUtilisateur.getDate("Entrez la date de publication (yyyy-MM-dd): ");
         List<Livre> result = arbreDate.rechercherParCritere(Livre::getDatePublication, datePublication);
         view.afficherLivres(result);
     }
 
-    public void afficherParPrixExact(double prix) {
+    public void afficherParPrixExact() {
+        double prix = ESUtilisateur.getDouble(0,Double.MAX_VALUE,"Quel est le prix ? : ");
         List<Livre> result = arbrePrix.rechercherParCritere(Livre::getPrix, prix);
         view.afficherLivres(result);
     }
 
-    public void afficherParCategorieEtPrix(String categorie, double prixMin, double prixMax) {
+    public void afficherParCategorieEtPrix() {
+
+        String categorie = ESUtilisateur.getText("Entrez la catégorie: ");
+        double prixMin = ESUtilisateur.getDouble(0,Double.MAX_VALUE,"Entrez le prix minimum: ");
+        double prixMax = ESUtilisateur.getDouble(0,Double.MAX_VALUE,"Entrez le prix minimum: ");
+
         List<Livre> result = new ArrayList<>();
         for (Livre livre : arbrePrix.parcoursInOrdre()) {
             if (livre.getCategorie().equalsIgnoreCase(categorie) &&
@@ -83,7 +94,10 @@ public class LivreController {
         view.afficherLivres(ruptureStock);
     }
 
-    public void afficherParAuteurEtTitre(String auteur, String debutTitre) {
+    public void afficherParAuteurEtTitre() {
+        String auteur = ESUtilisateur.getText("Entrez l'auteur: ");
+        String debutTitre = ESUtilisateur.getText("Entrez les lettres de début du titre: ");
+
         List<Livre> result = new ArrayList<>();
         for (Livre livre : arbreAuteur.rechercherParCritere(Livre::getAuteur, auteur)) {
             if (livre.getTitre().toLowerCase().startsWith(debutTitre.toLowerCase())) {
@@ -94,7 +108,11 @@ public class LivreController {
         view.afficherLivres(result);
     }
 
-    public void afficherParAuteurEtDate(String auteur, LocalDate datePublication) {
+    public void afficherParAuteurEtDate() {
+
+        String auteur = ESUtilisateur.getText("Entrez l'auteur: ");
+        LocalDate datePublication = ESUtilisateur.getDate("Quelle date de publication ? :");
+
         List<Livre> result = new ArrayList<>();
         for (Livre livre : arbreAuteur.rechercherParCritere(Livre::getAuteur, auteur)) {
             if (livre.getDatePublication().equals(datePublication)) {
@@ -121,5 +139,53 @@ public class LivreController {
         );
 
         view.afficherLivresHierarchie(hierarchie);
+    }
+
+    public void start() {
+        while (true) {
+            view.afficherMenu();
+            int choix = ESUtilisateur.getInt(1,11,"Choisissez une option : ");
+
+            switch (choix) {
+                case 1:
+                    afficherParCategorie();
+                    break;
+                case 2:
+
+                    afficherParAuteur();
+                    break;
+                case 3:
+
+                    afficherParTitre();
+                    break;
+                case 4:
+                    afficherParDatePublication();
+                    break;
+                case 5:
+                    afficherParPrixExact();
+                    break;
+                case 6:
+                    afficherLivresRuptureStock();
+                    break;
+                case 7:
+                     afficherParAuteurEtTitre();
+                    break;
+                case 8:
+                    afficherParAuteurEtDate();
+                    break;
+                case 9:
+                   afficherParCategorieEtPrix();
+                    break;
+                case 10:
+                    afficherLivresHierarchie();
+                    break;
+                case 11:
+                    System.out.println("Au revoir!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Option invalide, veuillez réessayer.");
+            }
+        }
     }
 }
